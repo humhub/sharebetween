cb5bcfb954784b3e6f9408cc0a6edccf947e0719
PatmanAA <31142000+PatmanAA@users.noreply.github.com>
2017-12-05 16:20:19 -0500

    Add files via upload

--- models/ShareForm.php
+++ models/ShareForm.php
@@ -1,31 +1,56 @@
 <?php
-
 namespace humhub\modules\sharebetween\models;
+
+use humhub\modules\space\models\Space;
+use humhub\modules\content\models\Content; //   Add 
+
 
 use Yii;
 use yii\base\Model;
-
 /**
- * @author Luke
- * @package humhub.modules_core.space.forms
- * @since 0.5
+ * Description of ShareForm
+ *
+ * @author buddha modif Patman
  */
 class ShareForm extends Model
 {
+        /**
+         * SpaceId selection array of the form.
+         * @var type
+         */
+        public $spaceSelection;
+        
+        /**
+         * Field for Invite GUIDs
+         *
+         * @var type
+         */
+
+        
+
 
     /**
-     * Field for Invite GUIDs
-     *
-     * @var type
+     * Returns an id => spacename array representation of the given $spaces array.
+     * @param array $spaces array of Space models
+     * @return type array in form of id => spacename
      */
-    public $space;
-
-    public function rules()
+        
+        public static function getSpaceItems(Content $content,$spaces = null)
     {
-        return array(
-            array('space', 'safe'),
-                //array('inviteExternal', 'checkInviteExternal'),
-        );
+        if($spaces == null) {
+            $spaces =  \humhub\modules\space\models\Membership::GetUserSpaces();
+        }
+        
+        $result = [];
+        foreach ($spaces as $space) {
+            if($content->container->guid != $space->guid)
+                $result[$space->guid] = $space->name;
+        }
+        return $result;
     }
+    
+  
+    
+    
 
 }
