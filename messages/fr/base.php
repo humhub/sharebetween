a5be22eee527b7b0761adc6ed1cbd041e851d06b
PatmanAA <31142000+PatmanAA@users.noreply.github.com>
2017-12-05 16:25:22 -0500

    Add files via upload

---
+++ messages/fr/base.php
@@ -0,0 +1,8 @@
+<?php
+return array (
+  'Share' => 'Partager',
+  '<strong>Share content</strong><BR><BR><span>Please select spaces</span>' => '<strong>Partager le contenu</strong><BR><BR><span>Selectionner des espaces</span>',
+  'Close' => 'Fermer',
+  'Content successfully shared.' => 'Contenu partagé.',
+    
+);
