1857b242cb2f1f78d625363c234b1ff2c421143c
Yuriy Bakhtin <yurybakh@gmail.com>
2023-03-15 09:31:01 +0300

    Check visibility of the shared content and link "Share"

--- widgets/views/wall-entry.php
+++ widgets/views/wall-entry.php
@@ -1,9 +1,12 @@
 <?php
 
-
-/** @var $share \humhub\modules\sharebetween\models\Share * */
-
 use humhub\modules\content\widgets\stream\StreamEntryWidget;
+use humhub\modules\sharebetween\models\Share;
 
+/* @var $share Share */
 ?>
-<?= StreamEntryWidget::renderStreamEntry($share->getContentRecord()); ?>
+<?php if ($share->getContentRecord()->content->canView()) : ?>
+    <?= StreamEntryWidget::renderStreamEntry($share->getContentRecord()) ?>
+<?php else : ?>
+    <?= Yii::t('SharebetweenModule.base', 'You don\'t have access to this content.') ?>
+<?php endif; ?>
