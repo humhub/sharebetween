61d08687f8e04edc4ca2bdbaa241487d6ae8d07f
HumHub Translations <hello@humhub.com>
2023-09-19 10:32:37 +0000

    Enh: Updated Translations (translate.humhub.org)

--- messages/hu/base.php
+++ messages/hu/base.php
@@ -1,13 +1,13 @@
 <?php
 return array (
-  '<strong>Share</strong> content' => '',
-  'Content not available' => '',
-  'Content you create is automatically displayed on your profile.' => '',
-  'Select Spaces here on which the content is to be additionally displayed.' => '',
-  'Share' => '',
-  'Share this content on your profile stream' => '',
-  'Shared content' => '',
+  '<strong>Share</strong> content' => 'Tartalom <strong>megosztása</strong>',
+  'Content not available' => 'A tartalom nem érhető el',
+  'Content you create is automatically displayed on your profile.' => 'Az Ön által létrehozott tartalom automatikusan megjelenik a profiljában.',
+  'Select Spaces here on which the content is to be additionally displayed.' => 'Válassza ki itt azokat a tereket, amelyeken a tartalmat meg kívánja jeleníteni.',
+  'Share' => 'Megosztás',
+  'Share this content on your profile stream' => 'Oszd meg ezt a tartalmat a profilfaladon',
+  'Shared content' => 'Megosztott tartalom',
   'Spaces' => 'Közösségek',
-  'This content has either been deleted or you no longer have permission to access it.' => '',
-  '{spaceName} by {userName}' => '',
+  'This content has either been deleted or you no longer have permission to access it.' => 'Ezt a tartalmat vagy törölték, vagy már nincs engedélye a hozzáféréshez.',
+  '{spaceName} by {userName}' => '{userName} {spaceName} közössége',
 );
