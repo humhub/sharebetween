39c191396abaa5ccc6915f1451ec783cf0149dcd
HumHub Translations <hello@humhub.com>
2024-01-24 19:31:50 +0000

    Enh: Updated Translations (translate.humhub.org)

--- messages/es/base.php
+++ messages/es/base.php
@@ -1,13 +1,13 @@
 <?php
 return array (
-  '<strong>Share</strong> content' => '',
-  'Content not available' => '',
-  'Content you create is automatically displayed on your profile.' => '',
-  'Select Spaces here on which the content is to be additionally displayed.' => '',
-  'Share' => '',
-  'Share this content on your profile stream' => '',
-  'Shared content' => '',
+  '<strong>Share</strong> content' => '<strong>Compartir</strong> contenido',
+  'Content not available' => 'Contenido no disponible',
+  'Content you create is automatically displayed on your profile.' => 'El contenido que usted cree será mostrado automáticamente en su perfil.',
+  'Select Spaces here on which the content is to be additionally displayed.' => 'Seleccione aquí Espacios en los cuales este contenido será también mostrado.',
+  'Share' => 'Compartir',
+  'Share this content on your profile stream' => 'Compartir este contenido en la cadena de publicaciones de su perfil',
+  'Shared content' => 'Compartir contenido',
   'Spaces' => 'Espacios',
-  'This content has either been deleted or you no longer have permission to access it.' => '',
-  '{spaceName} by {userName}' => '',
+  'This content has either been deleted or you no longer have permission to access it.' => 'Este contenido o ha sido borrado o ya no tiene permisos para accederlo.',
+  '{spaceName} by {userName}' => '{spaceName} de {userName}',
 );
--- messages/sv/base.php
+++ messages/sv/base.php
@@ -1,13 +1,13 @@
 <?php
 return array (
-  '<strong>Share</strong> content' => '',
-  'Content not available' => '',
-  'Content you create is automatically displayed on your profile.' => '',
-  'Select Spaces here on which the content is to be additionally displayed.' => '',
-  'Share' => '',
-  'Share this content on your profile stream' => '',
-  'Shared content' => '',
+  '<strong>Share</strong> content' => '<strong>Dela</strong> innehåll',
+  'Content not available' => 'Innehållet är inte tillgängligt',
+  'Content you create is automatically displayed on your profile.' => 'Innehåll du skapar visas automatiskt på din profil.',
+  'Select Spaces here on which the content is to be additionally displayed.' => 'Välj de eventuella forumytor där innehållet också ska visas.',
+  'Share' => 'Dela',
+  'Share this content on your profile stream' => 'Dela detta innehåll i flödet i din profil',
+  'Shared content' => 'Delat innehåll',
   'Spaces' => 'Forum',
-  'This content has either been deleted or you no longer have permission to access it.' => '',
-  '{spaceName} by {userName}' => '',
+  'This content has either been deleted or you no longer have permission to access it.' => 'Detta innehåll har antingen raderats eller så har du inte längre behörighet att komma åt det.',
+  '{spaceName} by {userName}' => '{spaceName} av {userName}',
 );
