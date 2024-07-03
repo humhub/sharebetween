b1304dbca7f39a79cacdce42128a5643a4ae9484
HumHub Translations <hello@humhub.com>
2024-07-03 09:47:51 +0000

    Enh: Updated Translations (translate.humhub.org)

--- messages/sk/base.php
+++ messages/sk/base.php
@@ -1,13 +1,13 @@
 <?php
 return array (
-  '<strong>Share</strong> content' => '',
-  'Content not available' => '',
-  'Content you create is automatically displayed on your profile.' => '',
-  'Select Spaces here on which the content is to be additionally displayed.' => '',
-  'Share' => '',
-  'Share this content on your profile stream' => '',
-  'Shared content' => '',
+  '<strong>Share</strong> content' => '<strong>Zdieľať</strong> obsah',
+  'Content not available' => 'Obsah nie je k dispozícii',
+  'Content you create is automatically displayed on your profile.' => 'Obsah, ktorý vytvoríte, sa automaticky zobrazí vo vašom profile.',
+  'Select Spaces here on which the content is to be additionally displayed.' => 'Tu vyberte Priestory, na ktorých sa má obsah dodatočne zobraziť.',
+  'Share' => 'Zdieľať',
+  'Share this content on your profile stream' => 'Zdieľajte tento obsah vo svojom profilovom prúde',
+  'Shared content' => 'Zdieľaný obsah',
   'Spaces' => 'Priestory',
-  'This content has either been deleted or you no longer have permission to access it.' => '',
-  '{spaceName} by {userName}' => '',
+  'This content has either been deleted or you no longer have permission to access it.' => 'Tento obsah bol buď odstránený, alebo už nemáte povolenie na prístup k nemu.',
+  '{spaceName} by {userName}' => '{spaceName} od používateľa {userName}',
 );
