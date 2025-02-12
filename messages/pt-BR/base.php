8cb7ffcf3c1f58877e47d6ac99cfdeb63fb8e3d3
HumHub Translations <hello@humhub.com>
2025-02-12 10:02:40 +0000

    Enh: Updated Translations (translate.humhub.org)

--- messages/pt-BR/base.php
+++ messages/pt-BR/base.php
@@ -1,13 +1,13 @@
 <?php
-return array (
+return [
   '<strong>Share</strong> content' => '<strong>Compartilhar</strong> conteúdo',
   'Content not available' => 'Conteúdo não disponível',
   'Content you create is automatically displayed on your profile.' => 'O conteúdo que você cria é exibido automaticamente no seu perfil.',
   'Select Spaces here on which the content is to be additionally displayed.' => 'Selecione aqui os espaços nos quais o conteúdo deve ser exibido adicionalmente.',
-  'Share' => 'Compartilhar',
+  'Share' => '➤',
   'Share this content on your profile stream' => 'Compartilhe este conteúdo no seu perfil',
   'Shared content' => 'Conteúdo compartilhado',
   'Spaces' => 'Espaços',
   'This content has either been deleted or you no longer have permission to access it.' => 'Este conteúdo foi excluído ou você não tem mais permissão para acessá-lo.',
   '{spaceName} by {userName}' => '{spaceName} por {userName}',
-);
+];
